3762a10135d012c62c62697af59bd17c680677f8
Dasha Belkina <37762064+dashabelkina@users.noreply.github.com>
2019-06-10 10:43:08 +0700

    21. Основное задание
    Форма с заказом отеля появляется и скрывается при нажатии на кнопку «Поиск гостиницы в Седоне»

---
+++ js/skript.js
@@ -0,0 +1,9 @@
+var link = document.querySelector(".search-button");
+var form = document.querySelector(".search-form");
+var date = form.querySelector("[name=date-checkin]");
+
+link.addEventListener("click", function (evt) {
+evt.preventDefault();
+form.classList.toggle("form-close");
+});
+date.focus();
